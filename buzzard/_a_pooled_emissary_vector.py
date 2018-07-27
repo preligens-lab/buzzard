a1f2f4a4b43f7e777d98fb025a66c5e83a5e3c25
Ngoguey42 <ngoguey@airware.com>
2018-07-27 17:02:01 +0200

    Add APooledEmissaryVector

---
+++ buzzard/_a_pooled_emissary_vector.py
@@ -0,0 +1,8 @@
+from buzzard._a_emissary_vector import *
+from buzzard._a_pooled_emissary import *
+
+class APooledEmissaryVector(APooledEmissary, AEmissaryVector):
+    pass
+
+class ABackPooledEmissaryVector(ABackPooledEmissary, ABackEmissaryVector):
+    pass
