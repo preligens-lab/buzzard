18b283a5bc97624ffdc9b868198a5d914ad83056
Ngoguey42 <ngoguey@airware.com>
2018-08-09 11:59:43 +0200

    Update _actor_caching.py

--- buzzard/_actor_caching.py
+++ buzzard/_actor_caching.py
@@ -11,7 +11,7 @@ class ActorCaching(object):
             cache_fp: _CacheTileStatus.unknown
             for cache_fp in raster.cache_fps
         }
-        self._queries = []
+        self._queries = {}
 
     def receive_ensure_cache_tiles_can_be_read(self, query_key, cache_fps):
         """Receive message: Ensure that those cache tiles can be read, if necessary create the
@@ -21,8 +21,8 @@ class ActorCaching(object):
         assert len(cache_fps) == len(set(cache_fps))
 
         msgs = []
-        query = _Query(cache_fps)
-        self._queries.append(query)
+        query = _Query(query_key, cache_fps)
+        self._queries[query_key] = query
 
         for cache_fp in cache_fps:
             status = self._cache_fps_status[cache_fp]
@@ -57,7 +57,8 @@ class ActorCaching(object):
         if len(query.cache_fps_ensured) != 0:
             # Notify the `Production` that those cache tiles are already ready
             msgs += [
-                Msg('Production' + query_key, 'cache_tiles_can_be_read', list(query.cache_fps_ensured))
+                Msg('Raster::Query:{}::Production'.format(query_key),
+                    'cache_tiles_can_be_read', list(query.cache_fps_ensured))
             ]
         if len(query.cache_fps_checking) == 0 and len(query.cache_fps_to_compute) > 0:
             # Some tiles need to be computed and none need to be checked, launching collection right
@@ -65,7 +66,7 @@ class ActorCaching(object):
             msgs += self._query_collection_ready(query)
         if query.is_done:
             # All cache tiles of query are now ensured to be ready
-            self._queries.remove(query)
+            del self._queries[query_key]
         return msgs
 
     def receive_done_one_cache_file_check(self, cache_fp, path, result):
@@ -74,7 +75,7 @@ class ActorCaching(object):
 
         # assertions
         assert self._cache_fps_status[cache_fp] == _CacheTileStatus.checking
-        for query in self._queries:
+        for query in self._queries.values():
             assert cache_fp not in query.cache_fps_ensured
             assert cache_fp not in query.cache_fps_to_compute
 
@@ -83,12 +84,13 @@ class ActorCaching(object):
             # - notify all waiting `Productions`
             # - launch collections if necessary
             self._cache_fps_status[cache_fp] = _CacheTileStatus.ready
-            for query in self._queries:
+            for query_key, query in self._queries.items():
                 if cache_fp in query.cache_fps_checking:
                     query.cache_fps_checking.remove(cache_fp)
                     query.cache_fps_ensured.add(cache_fp)
                     msgs += [
-                        Msg('Production' + query_key, 'cache_tiles_can_be_read', list(query.cache_fps_ensured))
+                        Msg('Raster::Query:{}::Production'.format(query_key),
+                            'cache_tiles_can_be_read', list(query.cache_fps_ensured))
                     ]
                     if len(query.cache_fps_checking) == 0 and len(query.cache_fps_to_compute) > 0:
                         msgs += self._query_collection_ready(query)
@@ -98,16 +100,16 @@ class ActorCaching(object):
             # - launch collections if necessary
             self._cache_fps_status[cache_fp] = _CacheTileStatus.absent
 
-            for query in self._queries:
+            for query in self._queries.values():
                 if cache_fp in query.cache_fps_checking:
                     query.cache_fps_checking.remove(cache_fp)
                     query.cache_fps_to_compute.add(cache_fp)
                     if len(query.cache_fps_checking) == 0 and len(query.cache_fps_to_compute) > 0:
                         msgs += self._query_collection_ready(query)
 
-        if query.is_done:
+        for query_key in [k for k, v in self._queries.items() if v.is_done]:
             # All cache tiles of query are now ensured to be ready
-            self._queries.remove(query)
+            del self._queries[query_key]
 
         return msgs
 
@@ -115,7 +117,7 @@ class ActorCaching(object):
         """Receive message: One cache tile was just written"""
         msgs = []
         assert self._cache_fps_status[cache_fp] == _CacheTileStatus.absent
-        for query in self._queries:
+        for query_key, query in self._queries.items():
             assert cache_fp not in query.cache_fps_ensured
             assert cache_fp not in query.cache_fps_checking
 
@@ -123,14 +125,20 @@ class ActorCaching(object):
                 query.cache_fps_to_compute.remove(cache_fp)
                 query.cache_fps_ensured.add(cache_fp)
                 msgs += [
-                    Msg('Production' + query_key, 'cache_tiles_can_be_read', list(query.cache_fps_ensured))
+                    Msg('Raster::Query:{}::Production'.format(query_key),
+                        'cache_tiles_can_be_read', list(query.cache_fps_ensured))
                 ]
 
-        if query.is_done:
+        for query_key in [k for k, v in self._queries.items() if v.is_done]:
             # All cache tiles of query are now ensured to be ready
-            self._queries.remove(query)
+            del self._queries[query_key]
 
         return msgs
+
+    def receive_query_dropped(self, query_key):
+        """Receive message: One query was dropped"""
+        if query_key in self._queries:
+            del self._queries[query_key]
 
     # ******************************************************************************************* **
     def _query_collection_ready(self, query):
@@ -147,7 +155,8 @@ class ActorCaching(object):
             for compute_fp in self.raster.compute_fps_of_cache_fp(cache_fp):
                 if compute_fp not in compute_fps:
                     compute_fps[compute_fp] = 42
-        return [Msg('Raster::Collection', 'schedule_collection', list(compute_fps.keys()))]
+        return [Msg('Raster::Collection', 'schedule_collection',
+                    query.query_key, list(compute_fps.keys()))]
 
 class _CacheTileStatus(enum.Enum):
     unknown = 1
@@ -156,7 +165,8 @@ class _CacheTileStatus(enum.Enum):
     ready = 4
 
 class _Query(object):
-    def __init__(self, cache_fps):
+    def __init__(self, query_key, cache_fps):
+        self.query_key = query_key
         self.cache_fps = tuple(cache_fps)
         self.cache_fps_checking = set()
         self.cache_fps_ensured = set()
