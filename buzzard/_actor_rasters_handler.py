d230bc94b0fb082affa8c02c51a71d5dddc5d6ad
Ngoguey42 <ngoguey@airware.com>
2018-08-22 11:22:52 +0200

    Add _actors/_pool_waiting_room.py, update _actor_rasters_handler.py

--- buzzard/_actor_rasters_handler.py
+++ buzzard/_actor_rasters_handler.py
@@ -1,34 +1,98 @@
+import collections
+
+from buzzard._actors._pool_waiting_room import import ActorPoolWaitingRoom
+from buzzard._actors._pool_working_room import import ActorPoolWorkingRoom
 
 class ActorRastersHandler(object):
-    """Actor that takes care of the lifetime of rasters' actors"""
+    """Actor that takes care of the lifetime of rasters' and pools' actors"""
     def __init__(self):
-        self.rasters = set()
-        self.actor_addresses_of_raster = {}
+        self._rasters = set()
+        self._actor_addresses_of_raster = {}
+        self._rasters_of_pool = collections.defaultdict(list)
+        self._actor_addresses_of_pool = {}
 
     address = '/RastersHandler'
 
     # ******************************************************************************************* **
-    def receive_external_new_raster(self, raster):
-        """Receive message sent by other thread: There is a new raster"""
+    def ext_receive_new_raster(self, raster):
+        """Receive message sent by something else than an actor, still treated synchronously: There
+        is a new raster
+        """
         msgs = []
-        self.rasters.add(raster)
+        self._rasters.add(raster)
+
+        # Instanciate raster's actors ******************************************
         actors = raster.create_actors()
-        self.actor_addresses_of_raster[raster] = [
+        msgs += actors
+        self._actor_addresses_of_raster[raster] = [
             actor.address
             for actor in actors
         ]
-        msgs += actors
+        del actors
+
+        # Instanciate pools' actors ********************************************
+        pools = {
+            id(pool): pool
+            for attr in [
+                'computation_pool', 'merge_pool', 'write_pool',
+                'file_checker_pool', 'read_pool', 'resample_pool',
+            ]
+            for pool in [getattr(raster, attr)]
+        }
+        for pool_id, pool in pools.items():
+            if pool_id not in self._rasters_of_pool:
+                actors = self._create_pool_actors(pool)
+                msgs += actors
+
+                self._actor_addresses_of_pool[pool_id] = [
+                    actor.address
+                    for actor in actors
+                ]
+
+            self._rasters_of_pool.append(raster)
+
         return msgs
 
-    def receive_external_kill_raster(self, raster):
-        """Receive message sent by other thread: A raster was closed"""
-        msgs = [
+    def ext_receive_kill_raster(self, raster):
+        """Receive message sent by something else than an actor, still treated synchronously: An
+        actor is closing
+        """
+        msgs = []
+        self._rasters.remove(raster)
+
+        # Deleting raster's actors *********************************************
+        msgs += [
             Msg(address, 'die')
-            for address in self.actor_addresses_of_raster[actor]
+            for address in self._actor_addresses_of_raster[actor]
         ]
-        del self.actor_addresses_of_raster[actor]
-        self.rasters.remove(raster)
+        del self._actor_addresses_of_raster[actor]
+
+        # Deleting pools' actors ***********************************************
+        pools = {
+            id(pool): pool
+            for attr in [
+                'computation_pool', 'merge_pool', 'write_pool',
+                'file_checker_pool', 'read_pool', 'resample_pool',
+            ]
+            for pool in [getattr(raster, attr)]
+        }
+        for pool_id, pool in pools.items():
+            self._rasters_of_pool[pool_id].remove(raster)
+            if len(self._rasters_of_pool) == 0:
+                del self._rasters_of_pool[pool_id]
+                msgs += [
+                    actor.address
+                    for actor in self._actor_addresses_of_pool[pool_id]
+                ]
+                del self._actor_addresses_of_pool[pool_id]
 
         return msgs
+
+    # ******************************************************************************************* **
+    def _create_pool_actors(self, pool):
+        return [
+            ActorPoolWaitingRoom(pool),
+            ActorPoolWorkingRoom(pool),
+        ]
 
     # ******************************************************************************************* **
---
+++ buzzard/_actors/_pool_waiting_room.py
@@ -0,0 +1,77 @@
+import itertools
+import operator
+import functools
+
+class ActorPoolWaitingRoom(object):
+    """Actor that takes care of prioritizing jobs waiting for space in a thread/process pool"""
+
+    def __init__(self, pool):
+        pool_id = id(pool)
+        self._pool_id = pool_id
+        self._token_count = pool._processes
+        short_id = short_id_of_id(pool_id)
+        self._tokens = {
+            # This has no particular meaning, the only hard requirement is just to have
+            # different tokens in a pool.
+            _PoolToken(short_id * 1000 + i)
+            for i in range(pool._processes)
+        }
+        self._all_tokens = set(self._tokens)
+        self._jobs = {}
+
+    @property
+    def address(self):
+        return '/Pool{}/WaitingRoom'.format(self._pool_id)
+
+    # ******************************************************************************************* **
+    def receive_schedule_job(self, job):
+        """Receive message: Schedule this job someday"""
+        self._jobs[job] = 42
+        return _schedule_jobs()
+
+    def receive_unschedule_job(self, job):
+        """Receive message: Forget about this waiting job"""
+        del self._jobs[jobs]
+        return []
+
+    def receive_global_priorities_update(self):
+        """Receive message: Update your heursitic data used to prioritize jobs"""
+        return []
+
+    def receive_salvage_token(self, token):
+        """Receive message: A Job is done/cancelled, allow some other job"""
+        assert token in self._all_tokens, 'Received a token that is not owned by this waiting room'
+        assert token not in self._tokens, 'Received a token that is already here'
+        self._tokens.add(token)
+        return _schedule_jobs()
+
+    # ******************************************************************************************* **
+    def _schedule_jobs(self):
+        if not self._tokens or not self._jobs:
+            return []
+        msgs = []
+
+        return msgs
+
+    # ******************************************************************************************* **
+
+class _PoolToken(int):
+    pass
+
+def grouper(iterable, n, fillvalue=None):
+    """itertools recipe: Collect data into fixed-length chunks or blocks
+    grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    """
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args, fillvalue=fillvalue)
+
+def short_id_of_id(id, max_digit_count=3):
+    id = int(id)
+    it = reversed(str(id))
+    it = grouper(it, max_digit_count, '0')
+    it = (
+        int(''.join(reversed(char_list)))
+        for char_list in it
+    )
+    short_id = functools.reduce(operator.xor, it)
+    return short_id
