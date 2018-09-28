cdd24658a5ffcf2471e03b4d8a3522dc1f6ab7b4
Ngoguey42 <ngoguey@airware.com>
2018-09-28 18:16:58 +0200

    Done with BackDataSourceSchedulerMixin

--- buzzard/_datasource_back_scheduler.py
+++ buzzard/_datasource_back_scheduler.py
@@ -1,15 +1,42 @@
 import collections
 import time
+import threading
 
 from buzzard._actors.top_level import ActorTopLevel
 from buzzard._actors.message import Msg, DroppableMsg
 
 class BackDataSourceSchedulerMixin(object):
 
-    def __init__(self, ):
-        pass
+    def __init__(self, ds_id):
+        self._ext_message_to_scheduler_queue = []
+        self._thread = None
+        self._ds_id = ds_id
+        self._stop = False
+        self._started = False
 
+    # Public methods **************************************************************************** **
+    def start_scheduler(self):
+        assert not self.started
+        self._thread = threading.Thread(
+            target=self._scheduler_loop_until_datasource_close,
+            name='DataSource{}Scheduler'.format(ds_id),
+            daemon=True,
+        )
+        self._thread.start()
+        self.started = True
+
+    def put_message(self, msg):
+        self._ext_message_to_scheduler_queue.append(msg)
+
+    def stop_scheduler(self):
+        assert not self._stop
+        self._stop = True
+
+    # Private methods *************************************************************************** **
     def _scheduler_loop_until_datasource_close(self):
+        """This is the entry point of a DataSource's scheduler.
+        The design of this method would be much better with recursive calls, but much slower too. (maybe)
+        """
 
         def _register_actor(a):
             if hasattr(a, 'ext_receive_nothing'):
@@ -22,7 +49,7 @@ class BackDataSourceSchedulerMixin(object):
             assert name not in actors[grp_name]
             actors[grp_name][name] = a
 
-        def _find_actor(self, address, relative_actor):
+        def _find_actor(address, relative_actor):
             names = address.split('/')
             if len(names) == 2:
                 return actors[names[0]].get(names[1])
@@ -33,16 +60,32 @@ class BackDataSourceSchedulerMixin(object):
                 assert False
 
         def _unregister_actor(a):
+            address = a.address
+            grp_name, name = address.split('/')
+            del actors[grp_name][name]
+            if not actors[grp_name]:
+                del actors[grp_name]
+            if hasattr(a, 'ext_receive_nothing'):
+                keep_alive_actors.remove(a)
 
+        # Dicts of actors
+        actors = collections.defaultdict(dict) # type: Mapping[str, Mapping[str, Actor]]
 
-        actors = collections.defaultdict(dict)
-        keep_alive_actors = [] # Should never be empty
-
-        top_level_actor = ActorTopLevel()
-        piles_of_msgs = [
-            (top_level_actor, top_level_actor.ext_receive_prime())
-        ] # type: List[Tuple[Actor, List[Union[Msg, Actor]]]]
+        # List of actors that need to be kept alive with calls to `ext_receive_nothing`
+        # `keep_alive_iterator` helps simulate
+        # `keep_alive_iterator` should never be iterated if `keep_alive_actors` is empty
+        keep_alive_actors = []
         keep_alive_iterator = _cycle_list(keep_alive_actors)
+
+        # Stack of pending messages
+        piles_of_msgs = [] # type: List[Tuple[Actor, List[Union[Msg, Actor]]]]
+
+        # Instanciate and register the top level actor
+        top_level_actor = ActorTopLevel()
+        _register_actor(top_level_actor)
+        piles_of_msgs.append(
+            (top_level_actor, top_level_actor.ext_receive_prime()),
+        )
 
         while True:
             # Step 1: Process all messages on flight
@@ -55,32 +98,64 @@ class BackDataSourceSchedulerMixin(object):
                 if isinstance(msg, Msg):
                     dst_actor = _find_actor(msg.address, src_actor)
                     if dst_actor is None:
+                        # This message may be discarted
                         assert isinstance(msg, DroppableMsg)
                     else:
                         new_msgs = getattr(dst_actor, 'receive_' + msg.title)(*msg.args)
+                        if self._stop:
+                            # DataSource is closing. This is the same as `step 5`
+                            return
+                        if not dst_actor.alive:
+                            # Actor is closing
+                            _unregister_actor(dst_actor)
                         if new_msgs:
+                            # Message need to be sent
                             piles_of_msgs.append((
                                 dst_actor, new_msgs
                             ))
                 else:
                     _register_actor(msg)
 
-            # Step 2: Find a "keep alive" actor that has messages to send
-            assert len(keep_alive_actors) != 0
-            for actor, _ in zip(keep_alive_iterator, range(len(keep_alive_actors))):
-                # Iter at most once on each "keep alive" actor
-                new_msgs = actor.ext_receive_nothing()
-                if not actor.alive:
+            # Step 2: Receive external messages
+            # a list is thread-safe: https://stackoverflow.com/a/6319267/4952173
+            if self._ext_message_to_scheduler_queue:
+                msg = self._ext_message_to_scheduler_queue.pop(0)
+                piles_of_msgs.append((
+                    _find_actor(msg, None), [msg]
+                ))
 
-                if new_msgs:
-                    piles_of_msgs.append((
-                        actor, new_msgs
-                    ))
-                    break
+            # Step 3: If no messages from phase 2 and some `keep_alive_actors`
+            #   Find "keep alive" actors that need to be closed
+            #   Find a "keep alive" actor that has messages to send
+            if keep_alive_actors and not piles_of_msgs:
+                actors_to_remove = []
+                for actor, _ in zip(keep_alive_iterator, range(len(keep_alive_actors))):
+                    # Iter at most once on each "keep alive" actor
+                    new_msgs = actor.ext_receive_nothing()
+                    if self._stop
+                        # DataSource is closing. This is the same as `step 5`
+                        return
+                    if not actor.alive:
+                        # Actor is closing
+                        actors_to_remove.append(actor)
+                    if new_msgs:
+                        # Messages need to be sent
+                        piles_of_msgs.append((
+                            actor, new_msgs
+                        ))
+                        break
+                for actor in actors_to_remove:
+                    _unregister_actor(actor)
 
-            # Step 3: Sleep if no messages from step 2
+            # Step 4: If no messages from phase 2 nor from phase 3
+            #   Sleep
             if not piles_of_msgs:
                 time.sleep(1 / 20)
+
+            # Step 5: Check if DataSource was collected
+            if self._stop:
+                return
+
 
 def _cycle_list(l):
     """Loop in a list forever, even if its size changes. Error if empty."""
