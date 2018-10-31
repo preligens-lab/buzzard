e4c523b7539daddbc03f34d052029df298e29a2b
Ngoguey42 <ngoguey@airware.com>
2018-10-31 14:37:45 +0100

    Improve part1.py and part2.py

--- doc/part1.py
+++ doc/part1.py
@@ -1,16 +1,18 @@
 """
-Part 1: A raster file configured to be read asynchronously
+# Part 1: A raster file configured to be read asynchronously
 
  By default in buzzard when calling `get_data()` on a raster file opened normally, all the data is read from disk at once (using one `gdal.Band.ReadAsArray()` for example), and all the optional resampling is performed in one step (using one `cv2.remap()` for example). When performing this operation on a large chunk of data, it would be much more efficient to read and resample __tile by tile to parallelize__ those tasks. To do so, use `scheduled=True` in `open_raster()` and `create_raster()`.
 
 Another feature unlocked by using a sheduled raster to read a file is the `iter_data()` method. Compared to the `get_data()` method that takes a _Footprint_ and return an _ndarray_, this new method takes a _list of Footprint_ and return an _iterator of ndarray_. By using this method the next array to be yielded is prepared in priority and the next ones are also prepared at the same time if there are enough workers available. You can control how much arrays can be made available in advance by setting the optional `max_queue_size=5` parameter of the `iter_data()` method, this allows you to __prevent backpressure__ if you consume the _iterator of ndarray_ too slowly.
 
-As seen before, the `scheduled` parameter can be a boolean, but instead of `True` you can also pass a _dict of options_ to parameterize how the raster is handled in the background. Some options control the amount of chunking to perform for the read and resampling steps, some other options allow you to choose the two _thread pools_ that will be used for reading and resampling. By default a single pool is shared by all rasters for _io_ operations (like reading a file), and another pool is shared by all rasters for cpu intensive operations (like resampling).
+As seen before, the `scheduled` parameter can be a _boolean_, but instead of `True` you can also pass a _dict of options_ to parameterize how the raster is handled in the background. Some options control the amount of chunking to perform for the read and resampling steps, some other options allow you to choose the two _thread pools_ that will be used for reading and resampling. By default a single pool is shared by all rasters for _io_ operations (like reading a file), and another pool is shared by all rasters for cpu intensive operations (like resampling).
 
 This kind of __ressource sharing__ between rasters is not trivial and requires some synchronization. To do so, a thread (called the _scheduler_) is spawned in the `DataSource` to manage the queries to rasters. As you will see in the next parts, the _scheduler_ is able to manage other kind of rasters.
+
 """
 
 import time
+import os
 
 import buzzard as buzz
 
@@ -26,7 +28,7 @@ def main():
     with ds.aopen_raster(path).close as r:
         test_raster(r)
 
-    # Opening with scheduler. Features:
+    # Opening within scheduler. Features:
     # - Disk reads are automatically tiled and parallelized
     # - Resampling operations are automatically tiled and parallelized
     # - `iter_data()` method is available
@@ -36,37 +38,44 @@ def main():
         # `scheduled={io_pool='io', resample_pool='cpu', max_resampling_size=512, max_read_size=512}`
         test_raster(r)
 
+    # `DataSource.close()` closes all rasters, the scheduler, and the pools.
+    # If you let the garbage collector collect the `DataSource`, the rasters and the scheduler will
+    # be correctly closed, but the pools will leak memory.
+    ds.close()
+
+    os.remove(path)
+
 def test_raster(r):
     """Basic testing functions. It will be reused throughout those tests"""
-    print('Test 1 - Print raster informations') # *************************** **
+    print('Test 1 - Print raster informations')
     fp = r.fp
-    fp_lowres = fp.intersection(fp, scale=fp.scale * 2)
     print(f'  type: {type(r)}')
     print(f'  dtype: {r.dtype}')
     print(f'  Footprint: center:{fp.c}, scale:{fp.scale}')
     print(f'             size:{fp.size}, raster-size:{fp.rsize}')
+    fp_lowres = fp.intersection(fp, scale=fp.scale * 2)
+    # ***************************************** **
 
     # Read the raster a first time to limit the impact of page caching in the
     # results. https://en.wikipedia.org/wiki/Page_cache
     r.get_data()
+    # ***************************************** **
 
-    print('Test 2 - Reading/computing the full raster') # ******************* **
+    print('Test 2 - Getting the full raster')
     with example_tools.Timer() as t:
         arr = r.get_data()
-    print('  array: px-width:{:.2f}m, dtype:{}, shape:{}, mean-value:{:3.3f}'.format(
-        fp.pxsizex, arr.dtype, arr.shape, arr.mean(),
-    ))
+    print_array_infos(fp, arr)
     print(f'  took {t}, {fp.rarea / float(t):_.0f}pixel/sec')
+    # ***************************************** **
 
-    print('Test 3 - Reading/computing and downsampling the full raster') # ** **
+    print('Test 3 - Getting and downsampling the full raster')
     with example_tools.Timer() as t:
         arr = r.get_data(fp=fp_lowres)
-    print('  array: px-width:{:.2f}m, dtype:{}, shape:{}, mean-value:{:3.3f}'.format(
-        fp_lowres.pxsizex, arr.dtype, arr.shape, arr.mean(),
-    ))
+    print_array_infos(fp_lowres, arr)
     print(f'  took {t}, {fp_lowres.rarea / float(t):_.0f}pixel/sec')
+    # ***************************************** **
 
-    print('Test 4 - Reading/computing the full raster in 9 tiles') # ******** **
+    print('Test 4 - Getting the full raster in 9 tiles with a slow main thread')
     tiles = fp.tile_count(3, 3, boundary_effect='shrink').flatten()
     if hasattr(r, 'iter_data'):
         # Using `iter_data` of scheduled rasters
@@ -79,10 +88,15 @@ def test_raster(r):
         )
     with example_tools.Timer() as t:
         for tile, arr in zip(tiles, arr_iterator):
-            print('  array: px-width:{:.2f}m, dtype:{}, shape:{}, mean-value:{:3.3f}'.format(
-                tile.pxsizex, arr.dtype, arr.shape, arr.mean(),
-            ))
+            print_array_infos(tile, arr)
+            time.sleep(1 / 9)
     print(f'  took {t}, {r.fp.rarea / float(t):_.0f}pixel/sec')
+    print_array_infos(fp_lowres, arr)
+    # ***************************************** **
+
+def print_array_infos(fp, arr):
+    format_ = '  array: px-width:{:.2f}m, dtype:{}, shape:{}, mean-value:{:3.3f}'
+    print(format_.format(fp.pxsizex, arr.dtype, arr.shape, arr.mean()))
 
 if __name__ == '__main__':
     main()
--- doc/part2.py
+++ doc/part2.py
@@ -1,59 +1,41 @@
+"""
+# Part 2: Deriving the slopes from a dem using a _raster recipe_
+In buzzard there exist 3 types of raster managed by the _DataSource_'s scheduler:
+- _GDALScheduledFileRaster_, seen in `Part 1`,
+- _RasterRecipe_, seen in this part,
+- _CachedRasterRecipe_, seen in `Part 4`.
+
+All those rasters are called _scheduled rasters_.
+
+### A new type of raster: _recipes_
+A _recipe_ is _scheduled raster_ that __computes data on the fly__ by calling the `compute_array` function provided in the constructor. This function takes a _Footprint_ the defines a rectangle to compute, and it return a _ndarray_ containing the pixels computed at this location. This function will be called in parallel given the `computation_pool` parameter provided in the constructor.
+
+A _recipe_ may __depend on some other scheduled rasters__. In this example, `ds.slopes` is a _RasterRecipe_ that depends on `ds.elevation` a _GDALScheduledFileRaster_. To declare the dependancy of `ds.slopes` on `ds.elevation`, in the constructor of `ds.slopes` you must provide `queue_data_per_primitive={'some_key': ds.elevation.queue_data}`, to allow the scheduler to issue queries to elevation when the slopes requires it. The `compute_array` function of `ds.slopes` will take as parameter the _ndarray_ of `ds.dem` previously extracted.
+
+A _recipe_ may depend on more than one _scheduled raster_, and a _recipe_ that depends on a _scheduled raster_ may also be needed by another recipe. This means that recipes can be assembled to form __computation graphs__ of any width and any depth.
+
+### Parallelization within _scheduled rasters_
+The computation intensive and io-bound steps of the scheduler are __defered to thread pools__ by default. You can configure the pools in the _scheduled raster_ constructors. Those parameters can be of the following types:
+- A _multiprocessing.pool.ThreadPool_, should be the default choice.
+- A _multiprocessing.pool.Pool_, a process pool. Useful for computations that require the [GIL](https://en.wikipedia.org/wiki/Global_interpreter_lock) or that leaks memory.
+- `None`, to request the scheduler thread to perform the tasks itself. Should be used when the computation is very light.
+- A _hashable_ (like a _string_), that will map to a pool registered in the _DataSource_. If that key is missing from the _DataSource_, a _ThreadPool_ with `multiprocessing.cpu_count()` workers will be automatically instanciated.
 
 """
-# Part 2: Deriving the slopes from a dem using a recipe
-There exist 3 types of scheduled rasters in buzzard
-- GDALScheduledFileRaster
-- RasterRecipe
-- CachedRasterRecipe
-A RasterRecipe is a scheduled raster that computes data on the fly, it may depends on some other scheduled rasters.
 
-In the following example, `ds.slopes` is a RasterRecipe that depends on `ds.elevation` a GDALScheduledFileRaster. To declare de dependancy of `ds.slopes` on `ds.elevation`, in the constructor of the slopes you must pass 'queue_data_per_primitive={'some_key': ds.elevation.queue_data}', to allow the scheduler to issue queries to elevation when the slopes requires it.
-
-A recipe may depend on multiple rasters, and a recipe that depends on a scheduled raster may also be needed by another recipe. This means that recipes can be assembled to form computation graphs of any width and any depth.
-<br/>
-
-The computation intensive and io-bound steps of the scheduler are defered to thread pools by default. You can configure the pools in the scheduled raster constructors. Those parameters can be the following:
-- A multiprocessing.pool.ThreadPool.
-- A multiprocessing.pool.Pool, a process pool.
-- None, to get the tasks performed by the scheduler
-- A hashable (like a string), that will map to a ThreadPool automatically instanciated
-<br/>
-
-The scheduler is designed to scale O(nlogn) in the number of active raters at once and in the number of queries active at once. Be careful though, the scheduler is coded in python and subject to the GIL, it is not designed for real time use cases. Porting the implementation to c++ is conceivable.
-"""
-
+import os
 import time
 import multiprocessing as mp
 import multiprocessing.pool
 
 import buzzard as buzz
-import scipy.ndimage
 import numpy as np
+import scipy.ndimage
 
 import example_tools
 from part1 import test_raster
 
-def slopes_of_elevation(fp, primitive_fps, primitive_arrays, slopes):
-   print('slopes_of_elevation from shape {} to {}'.format(
-       primitive_fps['dem'].shape, fp.shape,
-   ))
-   arr = primitive_arrays['dem']
-   kernel = [
-       [0, 1, 0],
-       [1, 1, 1],
-       [0, 1, 0],
-   ]
-   arr = (
-       scipy.ndimage.maximum_filter(arr, None, kernel) -
-       scipy.ndimage.minimum_filter(arr, None, kernel)
-   )
-   arr = arr[1:-1, 1:-1]
-   arr = np.arctan(arr / fp.pxsizex)
-   arr = arr / np.pi * 180.
-   return arr
-
-
-if __name__ == '__main__':
+def main():
     path = example_tools.create_random_elevation_gtiff()
     ds = buzz.DataSource()
 
@@ -73,6 +55,7 @@ if __name__ == '__main__':
     )
     ds.create_raster_recipe(
         'slopes',
+        computation_pool=cpu_pool,
 
         # The next 6 lines can be replaced by **buzz.algo?.slopes(ds.elevation)
         fp=ds.elevation.fp,
@@ -81,45 +64,70 @@ if __name__ == '__main__':
         compute_array=slopes_of_elevation,
         queue_data_per_primitive={'dem': ds.elevation.queue_data},
         convert_footprint_per_primitive={'dem': lambda fp: fp.dilate(1)},
-
-        computation_pool=cpu_pool,
     )
 
     # Test 1 - Perform basic tests ****************************************** **
     test_raster(ds.slopes)
 
     # Test 2 - Multiple iterations at the same time ************************* **
-    tiles = ds.elevation.fp.tile_count(2, 2)
-    dem_iterator = ds.elevation.iter_data(tiles.flat)
-    slopes_iterator = ds.slopes.iter_data(tiles.flat)
-    for tile, dem, slopes in zip(tiles.flat, dem_iterator, slopes_iterator):
-        print(f'Showing dem and slopes at {tile}')
+    tiles = ds.elevation.fp.tile_count(2, 2).flatten()
+    dem_iterator = ds.elevation.iter_data(tiles)
+    slopes_iterator = ds.slopes.iter_data(tiles)
+    for tile, dem, slopes in zip(tiles, dem_iterator, slopes_iterator):
+        print(f'Showing dem and slopes at:\n {tile}')
         example_tools.show_several_images(
             ('elevation (dem)', tile, dem),
             ('slopes', tile, slopes),
         )
 
     # Test 3 - Backpressure prevention ************************************** **
-    tiles = ds.slopes.tile_count(3, 3)
+    tiles = ds.slopes.tile_count(3, 3).flatten()
 
-    print('creating a slopes iterator on 9 tiles')
-    it = ds.slopes.iter_data(
-        tiles.flat, max_queue_size=1,
-    )
+    print('Creating a slopes iterator on 9 tiles')
+    it = ds.slopes.iter_data(tiles, max_queue_size=1)
+    print('  At most 5 dem arrays can be ready between `ds.elevation` and `ds.slopes`')
+    print('  At most 1 slopes array can be ready out of the slopes iterator')
 
-    print('sleeping to allow 6/9 dem arrays to be read, and 1/9 slopes array to be ready')
+    print('Sleeping to allow 6/9 dem arrays to be created, and 1/9 slopes array to be created')
     time.sleep(4)
 
     with example_tools.Timer() as t:
         arr = next(it)
-    print(f'getting the first array took {t}, this was instant because it was ready')
+    print(f'Getting the first array took {t}, this was instant because it was ready')
 
     with example_tools.Timer() as t:
         for _ in range(5):
             next(it)
-    print(f'getting the next 5 arrays took {t}, it was quick because the dems were ready')
+    print(f'Getting the next 5 arrays took {t}, it was quick because the dems were ready')
 
     with example_tools.Timer() as t:
         for _ in range(3):
             next(it)
-    print(f'getting the last 4 arrays took {t}, it was long because nothing was ready')
+    print(f'Getting the last 4 arrays took {t}, it was long because nothing was ready')
+
+    # Cleanup *********************************************************************************** **
+    ds.close()
+    os.remove(path)
+
+def slopes_of_elevation(fp, primitive_fps, primitive_arrays, slopes):
+    """A function to be fed to `compute_array` when constructing a recipe"""
+    print('slopes_of_elevation from shape {} to {}'.format(
+        primitive_fps['dem'].shape, fp.shape,
+    ))
+    arr = primitive_arrays['dem']
+    kernel = [
+        [0, 1, 0],
+        [1, 1, 1],
+        [0, 1, 0],
+    ]
+    arr = (
+        scipy.ndimage.maximum_filter(arr, None, kernel) -
+        scipy.ndimage.minimum_filter(arr, None, kernel)
+    )
+    arr = arr[1:-1, 1:-1]
+    arr = np.arctan(arr / fp.pxsizex)
+    arr = arr / np.pi * 180.
+    return arr
+
+if __name__ == '__main__':
+    main()
