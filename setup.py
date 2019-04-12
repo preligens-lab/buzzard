3dda4575e0d57d3adac03fbd9ed14dfb74320fef
Ngoguey42 <ngoguey@airware.com>
2019-04-12 10:48:57 +0200

    test requirements-ci.txt

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 import os
 import sys
+
 from setuptools import setup, find_packages
 
 # Requirements
 # https://caremad.io/posts/2013/07/setup-vs-requirement/
 reqs = [
-	'opencv-python',
-	'gdal',
-	'shapely',
-	'affine',
-	'scipy',
-	'pint',
-	'six',
-    'sortedcontainers',
-    'rtree',
+    'opencv-python>=3.1.0',
+    'gdal>=2.1.3',
+    'shapely>= 1.5.16',
+    'affine>=2.0.0',
+    'scipy>=0.19.1',
+    'pint>=0.8.1',
+    'six>=1.11.0',
+    'Rtree>=0.8.3',
+    'sortedcontainers>=1.5.9',
 ]
 
-
-if sys.version_info < (3, 5) and sys.version_info >= (3, 4):
+if (3, 4) <= sys.version_info < (3, 5):
     # https://github.com/scikit-image/scikit-image/issues/3820
     reqs += ['scikit-image<=0.14.1', 'numpy<1.16']
 else:
-    reqs += ['scikit-image', 'numpy']
+    reqs += ['scikit-image>=0.14.0', 'numpy>=1.15']
 
 if sys.version_info < (3, 2):
     reqs += ['chainmap>=1.0.2']
